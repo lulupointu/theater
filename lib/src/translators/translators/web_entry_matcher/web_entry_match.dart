a2979196007d3b7f5ed47e55ae2e0edadd7e0d0b
Lulupointu <lucasdelsol@hotmail.fr>
2022-01-26 17:16:44 +0100

    Make webentry public in match

--- lib/src/translators/translators/web_entry_matcher/web_entry_match.dart
+++ lib/src/translators/translators/web_entry_matcher/web_entry_match.dart
@@ -20,30 +20,30 @@ class WebEntryMatch {
   final Map<String, String> pathParams;
 
   /// See [WebEntry.path]
-  String get path => _webEntry.path;
+  String get path => webEntry.path;
 
   /// See [WebEntry.pathSegments]
-  List<String> get pathSegments => _webEntry.pathSegments;
+  List<String> get pathSegments => webEntry.pathSegments;
 
   /// See [WebEntry.fragment]
-  String get fragment => _webEntry.fragment;
+  String get fragment => webEntry.fragment;
 
   /// See [WebEntry.queryParams]
-  Map<String, String> get queryParams => _webEntry.queryParams;
+  Map<String, String> get queryParams => webEntry.queryParams;
 
   /// See [WebEntry.historyState]
-  Map<String, String> get historyState => _webEntry.historyState;
+  Map<String, String> get historyState => webEntry.historyState;
 
   /// The uri represents the url
   ///
   /// The historyState is not mandatory and will default to
   /// an empty map
   const WebEntryMatch({
-    required WebEntry webEntry,
+    required this. webEntry,
     required this.pathParams,
-  }) : _webEntry = webEntry;
+  });
 
   /// The [WebEntry] that was given to [WebEntryMatcher.match] when this
   /// [WebEntryMatch] was returned
-  final WebEntry _webEntry;
+  final WebEntry webEntry;
 }
